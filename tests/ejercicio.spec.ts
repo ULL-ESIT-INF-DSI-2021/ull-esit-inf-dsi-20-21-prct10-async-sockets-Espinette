67cc085b3a1f001ea66abe9b55c05a2d7520985a
Alberto Rios de la Rosa <alu0101235929@ull.edu.es>
2021-05-06 12:21:48 +0100

    comienzo a realizar la practica10

---
+++ tests/ejercicio.spec.ts
@@ -0,0 +1,8 @@
+import 'mocha';
+import {expect} from 'chai'; ;
+
+describe('Primera prueba', ()=> {
+  it('expect("hola").to.be.equal("hola");', ()=>{
+    expect("hola").to.be.equal("hola");
+  });
+});
