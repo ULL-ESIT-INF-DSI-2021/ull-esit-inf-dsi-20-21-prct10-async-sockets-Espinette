d2886d0734242284541b2e50c12dd6673d88dac5
Alberto Rios de la Rosa <alu0101235929@ull.edu.es>
2021-05-19 10:40:17 +0100

    Metodo Update implementado

---
+++ src/ej-clase-19-05/updateUser.ts
@@ -0,0 +1,32 @@
+import {MongoClient} from 'mongodb';
+
+const dbURL = 'mongodb://127.0.0.1:27017';
+const dbName = 'users';
+
+interface UserInterface {
+  nombre: string,
+  apellido: string,
+  correo: string,
+  edad?: number,
+  contraseña: string,
+}
+
+MongoClient.connect(dbURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then((client) => {
+  const db = client.db(dbName);
+
+  return db.collection<UserInterface>('users').updateOne({
+    correo: 'alu0101235929@ull.edu.es',
+  }, {
+    $set: {
+      edad: 21,
+      contraseña: '7654321',
+    },
+  });
+}).then((result) => {
+  console.log(result);
+}).catch((error) => {
+  console.log(error);
+});
